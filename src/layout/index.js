24737dad1129e4dbcc3ee59475dbdc5b79b96f56
Abrham Alemayehu <aaabrham421@gmail.com>
2022-12-05 12:54:19 +0300

    divided components as containers and ui

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
+import Footer from "../components/ui/Footer";
+import Navbar from "../components/ui/Navbar";
 
 const Layout = (props) => {
   return (
--- src/layout/pages/Home.js
+++ src/layout/pages/Home.js
@@ -1,4 +1,4 @@
-import Card from "../../components/Card";
+import Card from "../../components/ui/Card";
 
 const Home = (props) => {
   return (
