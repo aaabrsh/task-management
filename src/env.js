fdc89d8a34ab9c85db4f5a96c2319c89b443345f
Abrham Alemayehu <aaabrham421@gmail.com>
2023-01-23 13:38:20 +0300

    integrated boards get with api

---
+++ src/env.js
@@ -0,0 +1,1 @@
+export const api_url = "https://task-management-backend-virid.vercel.app/api"
--- src/reducers/boardSlice.js
+++ src/reducers/boardSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { api_url } from "../env";
 import { boards } from "../temp/boards";
 
 const initialState = [];
@@ -7,13 +8,27 @@ export const boardSlice = createSlice({
   name: "boards",
   initialState,
   reducers: {
-    fetchAllBoards: (state) => {
+    addBoards: (state) => {
       //redux toolkit automatically generate actions corresponding to the reducer names given here
-      return [...boards];
+      return [...state];
     },
   },
 });
 
-export const { fetchAllBoards } = boardSlice.actions;
+export const fetchBoards = () => async (dispatch) => {
+  try {
+    const response = await fetch(`${api_url}/boards`);
+    response = response.json();
+    if (response.success) {
+      dispatch(addBoards(response.data));
+    } else {
+      dispatch(addBoards([]));
+    }
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
+export const { addBoards } = boardSlice.actions;
 
 export default boardSlice.reducer;
