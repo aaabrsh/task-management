b9a5643828577ac9ed984590582512bf59520e2d
Abrham Alemayehu <aaabrham421@gmail.com>
2023-01-25 13:11:18 +0300

    added boards loading spinner

--- src/layout/pages/BoardsList.js
+++ src/layout/pages/BoardsList.js
@@ -1,28 +1,34 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import BoardCard from "../../components/ui/BoardCard";
+import Spinner from "../../components/ui/spinner";
 import { fetchBoards } from "../../reducers/boardSlice";
 
 function BoardsList() {
   let boards = useSelector((state) => state.boards);
+  const [spinner, setSpinner] = useState(false);
 
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchBoards());
+    setSpinner(true);
+    dispatch(fetchBoards()).then(() => setSpinner(false));
   }, []);
 
   return (
-    <div className="py-5 px-10">
+    <div className="py-5 px-10 h-full flex flex-col">
       <h1 className="p-5 bg-gray-300/20 text-lg">Your Boards</h1>
-      <div className="py-5 flex flex-wrap gap-4">
-        {boards.length > 0 ? (
-          boards.map((board) => <BoardCard board={board} key={board._id} />)
-        ) : (
-          <div className="block w-full text-lg text-center p-5">
-            No Boards Found
-          </div>
-        )}
+      <div className="py-5 relative">
+        {spinner && <Spinner />}
+        <div className="flex flex-wrap gap-4">
+          {boards.length > 0 ? (
+            boards.map((board) => <BoardCard board={board} key={board._id} />)
+          ) : (
+            <div className="block w-full text-lg text-center p-5">
+              No Boards Found
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
